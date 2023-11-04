89ed1948ec74bdf0fd771c069229160ce746a403
Yohan Le Gars <yohan.legars@gmail.com>
2023-11-04 17:38:52 +0100

    update sim launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -28,9 +28,24 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'yoyo_bot'],
                                    output='screen')
+    
+    # diff_drive_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner.py",
+    #     arguments=["diff_cont"],
+    # )
+
+    # joint_broad_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner.py",
+    #     arguments=["joint_broad"],
+    # )
 
     return LaunchDescription([
        rsp,
        gazebo,
        spawn_entity,
-   ]) 
+    ])
+#        diff_drive_spawner,
+#        joint_broad_spawner
+#    ]) 
